71c3638a557f4f3f1a2144c46dee5aca3b6d171b
Radu Ursache <radu@ursache.ro>
2021-08-10 19:50:53 +0300

    -

--- Sources/Models.swift
+++ Sources/Models.swift
@@ -6,12 +6,23 @@
 import UIKit
 
 public struct ExpandableItem {
-	public let name: String
-	public let detail: String
+	public var name: String
+	public var detail: String
+	
+	public init(name: String, detail: String) {
+		self.name = name
+		self.detail = detail
+	}
 }
 
 public struct ExpandableSection {
-	public let name: String
-	public let items: [ExpandableItem]
-	public var collapsed: Bool = false
+	public var name: String
+	public var items: [ExpandableItem]
+	public var collapsed: Bool
+	
+	public init(name: String, items: [ExpandableItem], collapsed: Bool = false) {
+		self.name = name
+		self.items = items
+		self.collapsed = collapsed
+	}
 }
